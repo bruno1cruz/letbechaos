a2977ccd2f09695d3c4e411750a999fcbe33cb7e
bruno.cruz <bruno.cruz@m4u.com.br>
2018-10-25 00:00:45 -0300

    add new relic agent

---
+++ app/index.js
@@ -0,0 +1,26 @@
+
+require('dotenv').load();
+require('newrelic');
+const http = require('http')
+var port = process.env.PORT || 5000
+
+const server = http.createServer((req, res) => {
+    if (req.url == '/fire') {
+        setTimeout(function () {
+            res.statusCode = 200
+            res.end();
+        }, Math.floor(Math.random()*1000))
+
+    } else if (req.url == '/health') {
+        res.statusCode = 200
+        res.end();
+    } else {
+        res.statusCode = 500
+        res.end();
+    }
+
+})
+
+server.listen(port, "localhost", () => {
+    console.log(`Servidor rodando em ${port}`)
+})
